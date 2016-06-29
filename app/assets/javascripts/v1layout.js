4cf11f505b6c41c97e32d89dbcce6bee89b13fe9
designernaruka <designernaruka@gmail.com>
2016-06-29 15:20:46 +0530

    design fixes for slider header done

--- app/assets/javascripts/v1layout.js
+++ app/assets/javascripts/v1layout.js
@@ -1,5 +1,17 @@
-//= require jquery
+//= require agency
 //= require bootstrap
 //= require cbpAnimatedHeader
 //= require classie
-//= require agency
+//= require jquery
+//= require swiper.min
+
+// For Touch Slider Carosol
+var swiper = new Swiper('.heroSlider', {
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  slidesPerView: 1,
+  paginationClickable: true,
+  spaceBetween: 30,
+  loop: true
+});
