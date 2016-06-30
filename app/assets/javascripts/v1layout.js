227d26328f2b2b41d62d27b6f5a10df75d05e24a
designernaruka <designernaruka@gmail.com>
2016-06-30 13:51:25 +0530

    hero slider added to code

--- app/assets/javascripts/v1layout.js
+++ app/assets/javascripts/v1layout.js
@@ -1,8 +1,9 @@
-//= require agency
 //= require bootstrap
+//= require jquery
 //= require cbpAnimatedHeader
 //= require classie
-//= require jquery
+//= require agency
+//= require jquery.easing.min
 //= require swiper.min
 
 // For Touch Slider Carosol
@@ -14,4 +15,44 @@ var swiper = new Swiper('.heroSlider', {
   paginationClickable: true,
   spaceBetween: 30,
   loop: true
+});
+
+
+// Add active on seciton on scroll and smooth scroll
+
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('.nav-link').removeClass('active');
+      sections.removeClass('active');
+
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+  target: '.navbar-fixed-top',
+  offset: 51
+})
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$('a.nav-links').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
 });
